9744337b87c5d46a0539c556fdf2a2d7fb4cc6a6
Firas Zaidan <firas@zaidan.de>
2018-04-10 01:18:08 +0200

    Add smcchar.ko to `gen_rootfs/filelist.txt`

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-SYSCASE=../syscase make KDIR=`pwd`/../../../linux ARCH=arm64 CROSS_COMPILE=`pwd`/../../../toolchains/aarch64/bin/aarch64-linux-gnu-
-cp smcchar.ko `pwd`/../../../gen_rootfs/etc/smcchar.ko
+root=$(dirname "$(readlink -f "$0")")
+rootfs="$root/../../../gen_rootfs"
+SYSCASE=../syscase make KDIR="$root/../../../linux" ARCH=arm64 CROSS_COMPILE="$root/../../../toolchains/aarch64/bin/aarch64-linux-gnu-"
+cp smcchar.ko "$rootfs/etc/smcchar.ko"
+
+filelist="$rootfs/filelist.txt"
+if [ "$(cat "$filelist" | grep smcchar.ko)" == "" ] ; then
+  echo "Adding smcchar.ko to 'gen_rootfs/filelist.txt'"
+  echo "file /etc/smcchar.ko etc/smcchar.ko 644 0 0" >> "$filelist"
+fi
