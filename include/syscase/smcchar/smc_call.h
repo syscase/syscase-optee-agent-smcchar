e1439c5efa51115076368b85d30c7816d40c8042
Firas Zaidan <firas@zaidan.de>
2018-04-25 20:27:17 +0200

    Change trace option to flags

--- include/syscase/smcchar/smc_call.h
+++ include/syscase/smcchar/smc_call.h
@@ -3,7 +3,7 @@
 
 #include "syscase/types.h"
 
-sc_u_long smc_call(char *input, sc_u_long input_size, int trace);
+sc_u_long smc_call(char *input, sc_u_long input_size, int flags);
 
 #endif /*SYSCASE_SMCCHAR_SMC_CALL_H*/
 
